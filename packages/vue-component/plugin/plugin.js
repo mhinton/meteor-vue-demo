0ff46e0bb4da13763f88f0548db22f4032f53fbf
Matthew Hinton <mhinton@gmail.com>
2018-11-29 10:36:44 -0600

    Remove patched packages

--- packages/vue-component/plugin/plugin.js
+++
@@ -1,7 +0,0 @@
-Plugin.registerCompiler({
-  extensions: ['vue'],
-  filenames: [IGNORE_FILE],
-}, () => new VueComponentCompiler())
-
-global.vue = global.vue || {}
-global.vue.lang = global.vue.lang || {}
